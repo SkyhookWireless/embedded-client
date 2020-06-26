bd19c0f12a73dd8f65bff476d818910d36b0a130
gcleary <gcleary@skyhook.com>
2020-06-26 13:43:45 -0400

    [EC-70] Configure libel for no cache and maximum beacons

--- libel/config.h
+++ libel/config.h
@@ -31,10 +31,10 @@
 
 /*! \brief The maximum number of beacons passed to the server in a request
  */
-#define TOTAL_BEACONS 11
+#define TOTAL_BEACONS 127
 /*! \brief The maximum number of AP beacons passed to the server in a request
  */
-#define MAX_AP_BEACONS 10
+#define MAX_AP_BEACONS 120
 
 /*! \brief The percentage of beacons that must match in a cached scan/location
  */
@@ -54,7 +54,7 @@
 
 /*! \brief The number of entries in the scan/response cache
  */
-#define CACHE_SIZE 1
+#define CACHE_SIZE 0
 
 /*! \brief The maximum space the dynamic configuration parameters may take up in bytes
  */
